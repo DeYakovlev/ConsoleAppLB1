eb1d9ee6c63c11fbffe6ce1819d47281c318365d
d.Yakovlev <d.Yakovlev@WS43>
2026-06-08 15:28:02 +0300

    Clean and Debag Z3

--- ConsoleAppLB1Z3/Z3.cs
+++ ConsoleAppLB1Z3/Z3.cs
@@ -1,7 +1,5 @@
 ﻿//Задание 3 Зубчатый массив. Вариант 16. Подсчитать сумму элементов каждой строки и записать данные в новый массив. Найти максимальный элемент нового массива..
 
-using System;
-
 int max, min, size;
 Random rand = new Random();
 int[][] arr ;
@@ -11,21 +9,21 @@ while (true) {
     Console.Write("Введите размер массива от 0 до 99: ");
     if (int.TryParse(Console.ReadLine(), out size) && size > 0 && size < 100)
         break;
-    Console.WriteLine("невнрное значение");
+    Console.WriteLine("неверное значение");
 }
 
 while (true)
 {
-    Console.Write("Введите минимальное занчение: ");
+    Console.Write("Введите минимальное значение: ");
     if (int.TryParse(Console.ReadLine(), out min))
         break;
-    Console.WriteLine("невнрное значение");
+    Console.WriteLine("неверное значение");
 }
 
 while (true)
 {
     Console.Write("Введите максимальное значение: ");
-    if (int.TryParse(Console.ReadLine(),out max))
+    if (int.TryParse(Console.ReadLine(),out max) && max > min)
         break;
     Console.WriteLine("неверное значение");
 }
@@ -37,10 +35,10 @@ for (int i = 0; i < size; i++)
 {
     int summ = 0;
     Console.WriteLine();
-    arr[i] = new int[rand.Next(1, size)];
+    arr[i] = new int[rand.Next(1, size + 1)];
     for (int j = 0; j < arr[i].Length; j++)
     {
-        arr[i][j] = rand.Next(min, max);
+        arr[i][j] = rand.Next(min, max + 1);
         summ += arr[i][j];
         Console.Write($" {arr[i][j]} ");
     }
@@ -49,9 +47,19 @@ for (int i = 0; i < size; i++)
 
 Console.WriteLine();
 
-for (int i = 0;i < size;i++)
+for (int i = 0; i < size;i++)
 {
     Console.Write($" {resArr[i]} ");
 }
 
+int maxSum = resArr[0];
 
+for (int i = 0;i < size;i++)
+{
+    if (resArr[i] > maxSum)
+        maxSum = resArr[i];
+}
+
+Console.WriteLine();
+
+Console.WriteLine($"Максимальная сумма строки: {maxSum}");    
