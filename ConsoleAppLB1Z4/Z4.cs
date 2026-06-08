4bfcf3d83a951a3e8441c23a99aeac05c5de19a6
d.Yakovlev <d.Yakovlev@WS43>
2026-06-08 15:38:26 +0300

    Clean and Debag Z4

--- ConsoleAppLB1Z4/Z4.cs
+++ ConsoleAppLB1Z4/Z4.cs
@@ -9,21 +9,21 @@ while (true)
     Console.Write("Введите размер массива от 0 до 99: ");
     if (int.TryParse(Console.ReadLine(), out size) && size > 0 && size < 100)
         break;
-    Console.WriteLine("невнрное значение");
+    Console.WriteLine("неверное значение");
 }
 
 while (true)
 {
-    Console.Write("Введите минимальное занчение: ");
+    Console.Write("Введите минимальное значение: ");
     if (int.TryParse(Console.ReadLine(), out min))
         break;
-    Console.WriteLine("невнрное значение");
+    Console.WriteLine("неверное значение");
 }
 
 while (true)
 {
     Console.Write("Введите максимальное значение: ");
-    if (int.TryParse(Console.ReadLine(), out max))
+    if (int.TryParse(Console.ReadLine(), out max) && max > min)
         break;
     Console.WriteLine("неверное значение");
 }
@@ -31,7 +31,7 @@ while (true)
 arr = new int[size];
 for (int i = 0; i < size; i++)
 {
-    arr[i] = rand.Next(min,max);
+    arr[i] = rand.Next(min,max + 1);
     Console.Write($" {arr[i]} ");
 }
 
@@ -48,21 +48,15 @@ for (int i = 0; i < arr.Length; i++)
 
 void method16(ref int[] arr)
 {
-    int size = arr.Length;
-    for (int i = 0; i < size; i++)
+    int count = arr.Length;
+    for (int i = 0; i < count; i++)
     {
         if (arr[i] < 0)
         {
-            Array.Copy(arr, i + 1, arr, i, size - i - 1);
-            size--;
+            Array.Copy(arr, i + 1, arr, i, count - i - 1);
+            count--;
             i--;
         }
     }
-    Array.Resize(ref arr, size);
+    Array.Resize(ref arr, count);
 }
-
-
-
-
-
-
