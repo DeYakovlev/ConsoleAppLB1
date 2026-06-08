a8bd7e1cf75857b7a079b2794bac8afeaa1d6a28
d.Yakovlev <d.Yakovlev@WS43>
2026-06-08 15:12:08 +0300

    clean and debag z2

--- ConsoleAppLB1Z2/Z2.cs
+++ ConsoleAppLB1Z2/Z2.cs
@@ -8,15 +8,14 @@ Random random = new Random();
 while (true)
 {
     Console.Write("Введите размерность массива от 0 до 99: ");
-    if (int.TryParse(Console.ReadLine(), out size) && size > 0 && size < 99)
+    if (int.TryParse(Console.ReadLine(), out size) && size > 0 && size <= 99)
         break;
-    //Console.Write($"{size}");
     Console.WriteLine($"Введенное значение size:{size} не попадает в допустимый диапазон");
 }
 
 while (true)
 {
-    Console.Write("Введите минимальное занчение: ");
+    Console.Write("Введите минимальное значение: ");
     if (int.TryParse(Console.ReadLine(), out min)) 
     break;
     Console.WriteLine("неверное значение");
@@ -24,7 +23,7 @@ while (true)
 
 while (true)
 {
-    Console.Write("Введите максимальное занчение: ");
+    Console.Write("Введите максимальное значение: ");
     if (int.TryParse(Console.ReadLine(), out max) && max > min)
         break;
     Console.WriteLine("неверное значение");
@@ -35,43 +34,44 @@ arr = new int[size][];
 for (int i = 0; i < size; i++)
 {
     arr[i] = new int[size];
-    if (i > 0)
-        Console.WriteLine();
 
     for (int j = 0; j < size; j++)
     {
-        arr[i][j] = random.Next(min, max);
+        arr[i][j] = random.Next(min, max + 1);
         Console.Write($" {arr[i][j]} ");
     }
+
+    Console.WriteLine();
 }
 
-bool Strispositive = false;
+bool strIsPositive = false;
 
-for  (int i = 0; i < size; i++)
+
+for (int i = 0; i < size; i++)
 {
-    if (Strispositive == false)
+    bool rowIsPositive = true;
+
+    for (int j = 0; j < size; j++)
     {
-        for (int j = 0; j < size; j++)
+        if (arr[i][j] <= 0)
         {
-            if (arr[i][j] > 0)
-                Strispositive = true;
-            else
-            {
-                Strispositive = false;
-                break;
-            }
-
-
+            rowIsPositive = false;
+            break;
         }
+
     }
-    else
+
+    if (rowIsPositive)
     {
+        strIsPositive = true;
         break;
     }
+    
 }
+
 Console.WriteLine();
 
-if (Strispositive == true)
+if (strIsPositive)
     Console.WriteLine("Есть строка только из положительных");
 else
     Console.WriteLine("Нет строк только из положительных");
